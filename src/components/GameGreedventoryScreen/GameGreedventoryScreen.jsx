d27e33c610edb488cad5e2abb70889b5b479b2d5
Nikita <belka.nik02@gmail.com>
2025-01-18 16:49:36 +0200

    Добавил ссылка в блок GreedVentory для открытия картинок с конффереций

--- src/components/GameGreedventoryScreen/GameGreedventoryScreen.jsx
+++ src/components/GameGreedventoryScreen/GameGreedventoryScreen.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import css from './GameGreedventoryScreen.module.css';
+import clsx from 'clsx';
 
 export default function GameGreedventoryScreen({ currentInfo }) {
   const greedInfo = currentInfo.fourGamesScreenGreedventory;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState('');
+
+  const openModal = (imageLink) => {
+    setModalImage(imageLink);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalImage('');
+    setIsModalOpen(false);
+  };
+
   return (
     <div className={css.gameGreedventoryScreen}>
       <h6 className={css.gamesScreenGeneralTextMarker}>{currentInfo.fourGamesScreenGeneralText}</h6>
@@ -30,6 +46,30 @@ export default function GameGreedventoryScreen({ currentInfo }) {
             </div>
             <h6 className={css.gameText}>{greedInfo.studioGame}</h6>
             <h6 className={css.gameText}>{greedInfo.descriptionGame}</h6>
+
+            <h6
+              className={clsx(css.gameText, css.gameTextLink)}
+              onClick={() => openModal(greedInfo.bestInnovationLink)}>
+              {greedInfo.bestInnovation}
+            </h6>
+            <h6
+              className={clsx(css.gameText, css.gameTextLink)}
+              onClick={() => openModal(greedInfo.bestGameOfTheShowLink)}>
+              {greedInfo.bestGameOfTheShow}
+            </h6>
+            {/* Modal for Image */}
+            {isModalOpen && (
+              <div className={css.modalOverlay} onClick={closeModal}>
+                <div className={css.modalContent} onClick={(e) => e.stopPropagation()}>
+                  <img
+                    src={modalImage}
+                    alt='Fullscreen Image'
+                    className={css.modalImage}
+                    onClick={closeModal}
+                  />
+                </div>
+              </div>
+            )}
           </div>
         </div>
       ) : (
